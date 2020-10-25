a19831cc2636f8cd258736ea392459c2f868f3a8
Deadida322 <61591520+Deadida322@users.noreply.github.com>
2020-10-25 21:22:59 +0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -238,16 +238,16 @@ if (document.documentElement.clientWidth <= 900){
         if (k == 1){
         $(".brush1").css({
             filter : 'blur(5px)',
-            transform: 'scale(1.5)'
+            transform: 'scale(1.1)'
         });
         $("h1").css({
-            filter : 'blur(10px)',
+            filter : 'blur(5px)',
             transform: 'scale(0.5)',
             opacity: '0'
 
         });
         $("h2").css({
-            filter : 'blur(10px)',
+            filter : 'blur(5px)',
             transform: 'scale(0.5)',
             opacity: '0'
         });
@@ -261,7 +261,7 @@ if (document.documentElement.clientWidth <= 900){
         $(".brush2").css({
             opacity: '100%',
             filter : 'blur(0px)',
-            transform: 'scale(1.5)'
+            transform: 'scale(1.3)'
         });
         $(".about1").css({
             opacity: '100%',
@@ -271,19 +271,19 @@ if (document.documentElement.clientWidth <= 900){
     }
     if (k == 2){
         $(".brush1").css({
-            filter : 'blur(10px)',
+            filter : 'blur(5px)',
             opacity: '0%',
             transform: 'scale(0.95)'
         });
         $(".brush2").css({
             opacity: '60%',
-            filter : 'blur(10px)',
+            filter : 'blur(5px)',
             transform: 'scale(1.0)'
         });
         $(".brush3").css({
             opacity: '100%',
             filter : 'blur(0px)',
-            transform: 'scale(1.7)'
+            transform: 'scale(1.3)'
         });
         $(".h31").css({
             filter : 'blur(10px)',
@@ -305,7 +305,7 @@ if (document.documentElement.clientWidth <= 900){
 
             $(".brush2").css({
                 opacity: '0%',
-                filter : 'blur(25px)',
+                filter : 'blur(3px)',
                 transform: 'scale(0.8)'
             });
             $(".brush1").css({
@@ -314,17 +314,17 @@ if (document.documentElement.clientWidth <= 900){
             });
             $(".brush3").css({
                 opacity: '100%',
-                filter : 'blur(10px)',
-                transform: 'scale(1.2)'
+                filter : 'blur(3px)',
+                transform: 'scale(1.1)'
             });
             $(".brush4").css({
                 opacity: '100%',
                 filter : 'blur(0px)',
-                transform: 'scale(1.2)'
+                transform: 'scale(1.1)'
             });
             $(".about2").css({
                 opacity: '00%',
-                filter : 'blur(15px)',
+                filter : 'blur(3px)',
                 transform: 'scale(0.7)'
             });
             $(".about3").css({
@@ -340,7 +340,7 @@ if (document.documentElement.clientWidth <= 900){
             k = 0;
             $(".brush1").css({
                 filter : 'blur(0px)',
-                transform: 'scale(2)',
+                transform: 'scale(1.7)',
                 opacity: '100%'
             });
             $("h1").css({
@@ -356,38 +356,38 @@ if (document.documentElement.clientWidth <= 900){
             });
             $(".brush2").css({
                 opacity: '0%',
-                filter : 'blur(10px)',
-                transform: 'scale(2)'
+                filter : 'blur(3px)',
+                transform: 'scale(1.7)'
             });
             $(".brush3").css({
                 opacity: '0%',
-                filter : 'blur(10px)',
-                transform: 'scale(2)'
+                filter : 'blur(3px)',
+                transform: 'scale(1.7)'
             });
             $(".brush4").css({
                 opacity: '0%',
-                filter : 'blur(10px)',
-                transform: 'scale(2.0)'
+                filter : 'blur(3px)',
+                transform: 'scale(1.7)'
             });
             $(".h31").css({
-                filter : 'blur(15px)',
-                transform: 'scale(2)',
+                filter : 'blur(3px)',
+                transform: 'scale(1.7)',
                 opacity: '00%'
             });
             $(".about1").css({
                 opacity: '0%',
-                filter : 'blur(15px)',
-                transform: 'scale(1.5)'
+                filter : 'blur(3px)',
+                transform: 'scale(1.3)'
             });
             $(".about2").css({
                 opacity: '00%',
-                filter : 'blur(15px)',
-                transform: 'scale(1.5)'
+                filter : 'blur(3px)',
+                transform: 'scale(1.3)'
             });
             $(".about3").css({
                 opacity: '00%',
-                filter : 'blur(10px)',
-                transform: 'scale(1.7)',
+                filter : 'blur(3px)',
+                transform: 'scale(1.5)',
                 zIndex : '0'
             });
             $(".arrow").attr("src", "assets/svg/Arrow.png");
